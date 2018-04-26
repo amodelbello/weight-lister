0888d1d2c02f7399b9c2d0241d68645dc360ac62
Amo DelBello <amodelbello@gmail.com>
2018-04-26 12:56:10 -0600

    Simplifies app component test by importing AppModule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,47 +1,25 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { TestBed, async } from '@angular/core/testing';
+import { AppModule } from './app.module';
+import { TestBed, async, fakeAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 
-import { FlashMessagesModule } from 'angular2-flash-messages';
-
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ExercisesComponent } from './components/exercises/exercises/exercises.component';
-import { WorkoutsComponent } from './components/workouts/workouts/workouts.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { LoginComponent } from './components/login/login.component';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AuthService } from './services/auth/auth.service';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        HeaderComponent,
-        FooterComponent,
-        DashboardComponent,
-        ExercisesComponent,
-        WorkoutsComponent,
-        NotFoundComponent,
-        LoginComponent,
       ],
       imports: [
-        AppRoutingModule,
-        FormsModule,
-        FlashMessagesModule.forRoot(),
+        AppModule
       ],
       providers: [
-        AuthService,
         { provide: APP_BASE_HREF, useValue : '/' },
-      ]
+      ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the app', (done) => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    expect(app.title).toBe('WeightLister');
+    done();
+  });
 });
