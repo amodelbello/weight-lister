0c393d9e87701c93b27e9b2050fd0aad8239cac6
Amo DelBello <amodelbello@gmail.com>
2018-04-27 12:05:51 -0600

    Adds dependencies to user guard spec file

--- src/app/guards/user.guard.spec.ts
+++ src/app/guards/user.guard.spec.ts
@@ -1,11 +1,26 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
+import { Router } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService} from '../services/auth/auth.service';
+import { StubAuthService } from '../services/auth/auth.service.stub';
+import { UserService } from '../services/user/user.service';
+import { StubUserService } from '../services/user/user.service.stub';
+
 import { UserGuard } from './user.guard';
 
 describe('UserGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UserGuard]
+      providers: [
+        UserGuard,
+        { provide: Router, useClass: class { navigate = jasmine.createSpy('navigate'); } },
+        { provide: UserService, useClass: StubUserService },
+        { provide: AuthService, useClass: StubAuthService },
+        { provide: AngularFirestore, useClass: class {}},
+        { provide: AngularFireAuth, useClass: class {}},
+      ]
     });
   });
 
