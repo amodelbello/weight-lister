619ea8d400e4c2f16c9a37a6443521e3f4804665
Amo DelBello <amodelbello@gmail.com>
2018-04-27 08:50:57 -0600

    Makes login component spec file pass

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,4 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FirebaseApp } from 'angularfire2'
+import { AngularFirestore } from 'angularfire2/firestore';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { AuthService} from '../../services/auth/auth.service';
+import { StubAuthService } from '../../services/auth/auth.service.stub';
+import { UserService } from '../../services/user/user.service';
+import { StubUserService } from '../../services/user/user.service.stub';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +20,21 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ 
+        LoginComponent 
+      ],
+      imports: [
+        FormsModule,
+      ],
+      providers: [
+        { provide: Router, useClass: class { navigate = jasmine.createSpy('navigate'); } },
+        { provide: UserService, useClass: StubUserService },
+        { provide: AuthService, useClass: StubAuthService },
+        { provide: AngularFireAuth, useClass: class {}},
+        { provide: AngularFirestore, useClass: class {}},
+        FirebaseApp,
+        FlashMessagesService,
+      ],
     })
     .compileComponents();
   }));
