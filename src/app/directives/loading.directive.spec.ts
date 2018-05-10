6b0da4d210917b35ddf87057dbb3f7abb7e8e879
Amo DelBello <amodelbello@gmail.com>
2018-05-09 22:44:02 -0600

    Adds tests for loading directive

--- src/app/directives/loading.directive.spec.ts
+++ src/app/directives/loading.directive.spec.ts
@@ -1,8 +1,53 @@
-import { LoadingDirective } from './loading.directive';
+import { Component, ComponentRef, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
-describe('LoadingDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new LoadingDirective();
-    // expect(directive).toBeTruthy();
+import { LoadingDirective } from './loading.directive';
+import { SpinnerComponent } from '../components/spinner/spinner.component';
+
+@Component({
+  template: `
+    <div *loading="true">
+      <p>Should not see this. Should see a spinner</p>
+    </div>
+    <div *loading="false">
+      <p>Not loading. Showing content.</p>
+    </div>
+  `
+})
+class TestComponent {}
+
+describe('Directive: LoadingDirective', () => {
+  let fixture: ComponentFixture<any>;
+  let el: DebugElement;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [
+        LoadingDirective, 
+        TestComponent,
+        SpinnerComponent,
+      ]
+    })
+    .overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [
+          SpinnerComponent,
+        ]
+      }
+    })
+    .createComponent(TestComponent);
+    el = fixture.nativeElement as DebugElement;
+    fixture.detectChanges();
+  });
+
+  afterEach(() => { fixture = null; });
+
+  it('should show spinner when loading.', () => {
+    expect(el['innerText']).not.toContain('Should not see this. Should see a spinner');
+  });
+
+  it('should not show spinner when not loading.', () => {
+    expect(el['innerText']).toContain('Not loading. Showing content.');
   });
 });
