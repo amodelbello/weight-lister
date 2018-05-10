69dafd04483a1811cca11953b01573d244be3181
Amo DelBello <amodelbello@gmail.com>
2018-05-10 12:52:45 -0600

    Improvement to title-case pipe tests

--- src/app/pipes/title-case.pipe.spec.ts
+++ src/app/pipes/title-case.pipe.spec.ts
@@ -16,17 +16,17 @@ describe('TitleCasePipe', () => {
   });
 
   it ('should take a string and convert it to title case', () => {
-    const testStrng = "this is a test string to convert to title case";
+    const testStrng = "and this is a test string to convert to title case";
     const transformedString = pipe.transform(testStrng);
-    const expectedResult = 'This Is a Test String to Convert to Title Case';
+    const expectedResult = 'And This Is a Test String to Convert to Title Case';
 
     expect(transformedString).toBe(expectedResult);
   });
 
   it ('should take a string and apply capital case to all words', () => {
-    const testStrng = "this is a test string to convert to title case";
+    const testStrng = "and this is a test string to convert to title case";
     const transformedString = pipe.transform(testStrng, true);
-    const expectedResult = 'This Is A Test String To Convert To Title Case';
+    const expectedResult = 'And This Is A Test String To Convert To Title Case';
 
     expect(transformedString).toBe(expectedResult);
   });
